e2c0a497a7876810c96f510b61e4dc9b04af9d1b
muslihk_diy <muslihk00@gmail.com>
2022-11-26 07:30:53 +0530

    initial commit

--- PyDialer/apps/asterisk_originates.py
+++ PyDialer/apps/asterisk_originates.py
@@ -7,12 +7,13 @@ _HOST = 'localhost'
 _USERNAME = 'admin'
 _PASSWORD = '642d10ac25fe5738a35cdeee77244cf3'
 
-def originate(endpoint,context='from-internal',extension='s',priority=1):
+def originate(endpoint,callerid=None,context='from-internal',extension='s',priority=1):
+    if not callerid:callerid=f'"{endpoint}"<{endpoint}>'
     manager = pystrix.ami.Manager()
     manager.connect(_HOST)
     challenge_response = manager.send_action(pystrix.ami.core.Challenge())
     action = pystrix.ami.core.Login(
         _USERNAME, _PASSWORD, challenge=challenge_response.result['Challenge'])
     manager.send_action(action)
-    manager.send_action(Originate_Context(channel=f'PJSIP/{endpoint}', context=context, extension=extension,priority=priority))
+    manager.send_action(Originate_Context(channel=f'PJSIP/{endpoint}',callerid=callerid,context=context, extension=extension,priority=priority))
     manager.send_action(pystrix.ami.core.Logoff())
